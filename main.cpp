2f2e043de02ad9cd486f6b001198534a3c564384
reiden <35837918+pepepper@users.noreply.github.com>
2020-04-17 02:39:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <vector>
+#include <random>
+#include <string>
+#include <iostream>
 
 int main(){
-start:char matrix[13]={"マトリックス"};
-	char result[13]={NULL};
-	char ret[128];
-srand((unsigned) time(NULL));
-for(int i=0;i<12;i=i+2){
-	int x=rand()%6;
-result[i]=matrix[x*2];
-result[i+1]=matrix[x*2+1];
-}
-result[12]=NULL;
-printf("%s",result);
-cont:printf("\nまだ生成する? (y/n) ");
-scanf("%s",ret);
-if(strstr(ret,"y"))
-goto start;
-else if(strstr(ret,"n"))
+        bool cont=true;
+        std::vector<std::string> matrix={"マ","ト","リ","ッ","ク","ス"};
+        std::string output,input;
+        std::random_device seed_gen;
+        std::default_random_engine engine(seed_gen());
+        std::uniform_int_distribution<> dist(0, matrix.size()-1);
+        while(cont){
+        output="";
+                for(unsigned int i=0;i<matrix.size();i++){
+                        output+=matrix[dist(engine)];
+                }
+                std::cout<<output<<std::endl;
+                bool valid=false;
+                while(!valid){
+                        std::cout<<"まだ生成する? (y/n) ";
+                        std::cin>>input;
+                        if(input.find("y")!=std::string::npos){
+                                cont=true;
+                                valid=true;
+                        }else if(input.find("n")!=std::string::npos){
+                                cont=false;
+                                valid=true;
+                        }else valid=false;
+                }
+        }
 return 0;
-else
-goto cont;
 }
